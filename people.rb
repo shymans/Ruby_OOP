7a9386e4ab1e28fbfd629134c7a973aceffdf3b7
shymans <austin.shymanski@gmail.com>
2013-10-23 10:27:32 -0400

    working classtime

--- people.rb
+++ people.rb
@@ -1,22 +1,37 @@
-class Student
+class Person
+  def initialize(name)
+    @name = name
+  end
+
+  def welcome
+    puts "Hi, my name is #{@name}"
+  end
+end
+
+class Student < Person
   def learn
     puts "I get it!"
   end
 end
 
-class Instructor
+class Instructor < Person
   def teach
-    puts "Everything in Ruby is an object"
+    puts "Everything in Ruby is an object!"
   end
 end
 
-class Person
-    def name
-      @name
-    end
+chris = Instructor.new("Chris")
+chris.welcome
+christina = Student.new("Christina")
+christina.welcome
+chris.teach
+christina.learn
 
-    def name=(str)
-      @name = str
-    end
-end
+#chris.learn
+#christina.teach
 
+=begin
+Calling chris.learn and christina.teach will not work because we did not
+initialize it, meaning that it is an undefined method. If we added each 
+of them to the student class or the teacher classthen they would work fine.
+=end
