f09a0761cecf0b4a1acefa4b209de74f8f1c1f0c
shymans <austin.shymanski@gmail.com>
2013-10-23 09:56:02 -0400

    starting to build classes

---
+++ people.rb
@@ -0,0 +1,22 @@
+class Student
+  def learn
+    puts "I get it!"
+  end
+end
+
+class Instructor
+  def teach
+    puts "Everything in Ruby is an object"
+  end
+end
+
+class Person
+    def name
+      @name
+    end
+
+    def name=(str)
+      @name = str
+    end
+end
+
